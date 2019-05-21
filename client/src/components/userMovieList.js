fc14ca49f0959e41cfd8eea75357fe42d4c14f84
Mike Starbuck <miglas9@gmail.com>
2019-05-21 15:43:31 -0500

    added json data file to client to test mapping

--- client/src/components/userMovieList.js
+++ client/src/components/userMovieList.js
@@ -1,23 +1,23 @@
 import React, { Component } from 'react';
 // import { Link } from 'react-router-dom';
 import { RICHARD_UNOGS_KEY, MIKE_UNOGS_KEY } from '../.env.json';
-
-
+import movieData from '../movieDataTest.json'
 export class MovieList extends Component {
     constructor() {
         super()
         this.state = {
-            movies: [
-                {
-                    imdbid: 'tt3114390',
-                    title: 'Kill la Kill',
-                    expiration: '2019-05-22',
-                    synopsis: 'woman with the scissor blade&#39; -- Ryuko Matoi -- searches for clues all around the world for the truth behind her father',
-                    released: '2013',
-                    imageURL: 'http://occ-0-2433-2705.1.nflxso.net/dnm/api/v6/XsrytRUxks8BtTRf9HNlZkW2tvY/AAAABXMs-AB4iYgHqDaxTVJOaSE-rVpAJlh3q8TP4htG4pH7KI4pGP_NE0EtH5DC8sWgS0cQcUGSYOorxgORb7E3m5QS_x7CAo4.jpg?r=206',
-                    rating: '7.9'
-                }
-            ],
+            movies: movieData,
+            // movies: [
+            //     {
+            //         imdbid: 'tt3114390',
+            //         title: 'Kill la Kill',
+            //         expiration: '2019-05-22',
+            //         synopsis: 'woman with the scissor blade&#39; -- Ryuko Matoi -- searches for clues all around the world for the truth behind her father',
+            //         released: '2013',
+            //         imageURL: 'http://occ-0-2433-2705.1.nflxso.net/dnm/api/v6/XsrytRUxks8BtTRf9HNlZkW2tvY/AAAABXMs-AB4iYgHqDaxTVJOaSE-rVpAJlh3q8TP4htG4pH7KI4pGP_NE0EtH5DC8sWgS0cQcUGSYOorxgORb7E3m5QS_x7CAo4.jpg?r=206',
+            //         rating: '7.9'
+            //     }
+            // ],
             imdbid: '',
             title: '',
             expiration: '',
@@ -28,12 +28,12 @@ export class MovieList extends Component {
         }
     }
     render() {
-        let movies = this.state.movies
+        let movies = this.state.movies.ITEMS
         let movieItem = movies.map((movie) => {
             return (
                 <li key={movie.imdbid}>
                     <div>
-                        <img src={movie.imageURL}></img>
+                        <img src={movie.image}></img>
                         <p>{movie.title}</p>
                         <p>{movie.expiration}</p>
                         <p>Countdown</p>
