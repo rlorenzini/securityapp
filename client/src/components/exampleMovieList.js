846821fd6770d7b0f1447315616c101d73333e1d
Richard Lorenzini <richardlorenzini@yahoo.com>
2019-05-28 11:57:05 -0500

    oops again

--- client/src/components/exampleMovieList.js
+++ client/src/components/exampleMovieList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import handleCountdown from './utils/handleCountdown'
 import './styling/movieList.css'
 import replaceASCII from './utils/replaceASCII'
+import movieData from '../movieData.json'
 
 export class ExampleMovieList extends Component {
   constructor(props) {
