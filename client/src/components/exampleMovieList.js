1b8eecdb128ad533446347a58a5306d856b3c011
Mike Starbuck <miglas9@gmail.com>
2019-05-30 10:26:08 -0500

    tweak example watch list

--- client/src/components/exampleMovieList.js
+++ client/src/components/exampleMovieList.js
@@ -5,53 +5,55 @@ import replaceASCII from './utils/replaceASCII'
 import movieData from '../movieData.json'
 
 export class ExampleMovieList extends Component {
-  constructor(props) {
-      super(props)
-      this.state = {
-          movies: ''
-      }
-  }
-  componentDidMount() {
-      let url = "http://localhost:8080/expiring"
-      fetch(url, {headers: {
-      "Content-type": "application/json; charset=UTF-8"
-    }})
-          .then(response => response.json())
-          .then(json => {
-              this.setState({
-                  ...this.state.movies,
-                  movies: json
-              })
-              localStorage.setItem('movieData', JSON.stringify(json))
-          })
-  }
-  render() {
-      let movies = []
-      movies = JSON.parse(localStorage.getItem('movieData')).ITEMS
-      let movieItem = movies.map((movie) => {
-        let str = replaceASCII(movie.title)
-          return (
-              <div className="listElementContainer">
-                  <li className="listElement" key={movie.imdbid}>
-                      <div>
-                          <img src={movie.image} alt={movie.image}></img>
-                          <p className="listElementMovieTitle">{str}</p>
-                          <p className="listElementMovieEndDate">{movie.unogsdate}</p>
-                          <span className="listElementMovieCountdown">{(handleCountdown(movie.unogsdate) === 0) ? <p className="noLongerAvailable">No Longer Available</p> : (handleCountdown(movie.unogsdate) === 1) ? <p className="lastDayToWatch">Last Day to Watch</p> : `${handleCountdown(movie.unogsdate)} days remaining`}</span>
-                      </div>
-                  </li>
-              </div>
+    constructor(props) {
+        super(props)
+        this.state = {
+            movies: ''
+        }
+    }
+    componentDidMount() {
+        let url = "http://localhost:8080/expiring"
+        fetch(url, {
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        })
+            .then(response => response.json())
+            .then(json => {
+                this.setState({
+                    ...this.state.movies,
+                    movies: json
+                })
+                localStorage.setItem('movieData', JSON.stringify(json))
+            })
+    }
+    render() {
+        let movies = []
+        movies = JSON.parse(localStorage.getItem('movieData')).ITEMS
+        let movieItem = movies.map((movie) => {
+            let str = replaceASCII(movie.title)
+            return (
+                <div className="listElementContainer">
+                    <li className="listElement" key={movie.imdbid}>
+                        <div>
+                            <img src={movie.image} alt={movie.image}></img>
+                            <p className="listElementMovieTitle">{str}</p>
+                            <p className="listElementMovieEndDate">{movie.unogsdate}</p>
+                            <span className="listElementMovieCountdown">{(handleCountdown(movie.unogsdate) <= 0) ? <p className="noLongerAvailable">No Longer Available</p> : (handleCountdown(movie.unogsdate) === 1) ? <p className="lastDayToWatch">Last Day to Watch</p> : `${handleCountdown(movie.unogsdate)} days remaining`}</span>
+                        </div>
+                    </li>
+                </div>
 
-          )
+            )
 
-      })
-      return (
-          <div className="listElementBody">
-            <div className="listElementHeader">
-            <h1>This is What's Leaving</h1>
+        })
+        return (
+            <div className="listElementBody">
+                <div className="listElementHeader">
+                    <h1>This is What's Leaving</h1>
+                </div>
+                <ul className="movieItemList">{movieItem}</ul>
             </div>
-            <ul className="movieItemList">{movieItem}</ul>
-          </div>
-      )
-  }
+        )
+    }
 }
